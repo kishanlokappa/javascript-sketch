fdf86cef7941e031fa5bd7326f2a838c7d2f8633
KISHAN L <kishan.karthi7@gmail.com>
2021-07-24 13:05:01 +0200

    wrapup javscript sketch

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,8 +3,14 @@ console.log('it works');
 // select canvas , shake button, create context
 const canvas = document.querySelector(`#etch-a-sketch`);
 const shake = document.querySelector(`.shake`);
+const MOVE_AMOUNT = 10;
 
 const ctx = canvas.getContext(`2d`);
+
+//set hue for line color
+let hue = 0;
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+
 
 // setup context to draw
 const width = canvas.width;
@@ -21,3 +27,66 @@ ctx.beginPath();    //  Starts drawing
 ctx.moveTo(x, y);
 ctx.lineTo(x, y);
 ctx.stroke();
+
+//  write draw function
+function draw(options) {
+
+    hue = hue + 1;
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+
+
+    switch (options.key) {
+        case 'ArrowUp':
+            y = y - MOVE_AMOUNT;
+            break;
+
+        case 'ArrowDown':
+            y = y + MOVE_AMOUNT;
+            break;
+
+        case 'ArrowLeft':
+            x = x - MOVE_AMOUNT;
+            break;
+
+        case 'ArrowRight':
+            x = x + MOVE_AMOUNT;
+            break;
+
+
+        default:
+            break;
+    }
+    ctx.lineTo(x, y);
+    ctx.stroke();
+
+}
+
+//  write handler for the keys
+function handleKey(event) {
+    if (event.key.includes('Arrow')) {
+        event.preventDefault();
+        draw({ key: event.key });
+    }
+}
+
+//  clear / shake function
+shake.addEventListener('click', clearCanvas)
+
+function clearCanvas() {
+
+
+    canvas.classList.add('shake');
+
+    ctx.clearRect(0, 0, width, height); // clears canvas
+
+    canvas.addEventListener('animationend', function () {
+        console.log('shake that booty');
+        canvas.classList.remove('shake');
+    }, { once: true })
+}
+
+//  Listen for the arrow keys
+window.addEventListener('keydown', handleKey);
